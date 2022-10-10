a94bbb3ea4186269a58847267ba9c65d496b0e5b
LeadMarker <72586064+LeadMarker@users.noreply.github.com>
2022-10-09 22:21:56 -0400

    Update furry_talk.lua

--- furry_talk.lua
+++ furry_talk.lua
@@ -1,5 +1,6 @@
 getgenv().furry_talk = true 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/StepBroFurious/Script/main/Text2Furry.lua"))()
+-- https://v3rmillion.net/member.php?action=profile&uid=485191 (StepBroFurious)
 
 local old; old = hookmetamethod(game, '__namecall', function(self, ...)
     local method, args = getnamecallmethod(), {...}
