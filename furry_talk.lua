caf7206174b6a4e9c000e0fd57ac46bed49aa03c
LeadMarker <72586064+LeadMarker@users.noreply.github.com>
2022-10-09 22:20:49 -0400

    Create furry_talk.lua

---
+++ furry_talk.lua
@@ -0,0 +1,15 @@
+getgenv().furry_talk = true 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/StepBroFurious/Script/main/Text2Furry.lua"))()
+
+local old; old = hookmetamethod(game, '__namecall', function(self, ...)
+    local method, args = getnamecallmethod(), {...}
+    
+    if method == 'FireServer' and self.Name == 'SayMessageRequest' and args[1] and getgenv().furry_talk then 
+        local msg = args[1] 
+        args[1] = encode(msg)
+        
+        return old(self, unpack(args))
+    end
+    
+    return old(self, ...)
+end)
