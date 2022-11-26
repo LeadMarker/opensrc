de3c1359df9f42bfe6f5cf9ce6656e55f6f4852d
LeadMarker <72586064+LeadMarker@users.noreply.github.com>
2022-11-26 06:06:05 -0500

    Update esp.lua

--- Deadline/esp.lua
+++ Deadline/esp.lua
@@ -1,9 +1,7 @@
--- game is annoying and cba to fully make it
-
 -- // Vars \\ -- 
 local client = game:GetService('Players').LocalPlayer
 local characters = game:GetService('Workspace').characters
-local random_player, holder = tostring(game:GetService('Workspace').characters:GetChildren()[1])
+local random_player = tostring(game:GetService('Workspace').characters:GetChildren()[1])
 local camera = game:GetService('Workspace').CurrentCamera
 
 -- // Character Stuff \\ -- 
@@ -12,21 +10,13 @@ for i,v in pairs(getgc(true)) do
         holder = v 
         break
     end
-end
-
-local function sort_table(tab)
-    local new_table = {}
     
-    for i,v in pairs(tab) do 
-        if characters:FindFirstChild(i) then 
-            new_table[i] = v
+    if type(v) == 'table' and rawget(v, client.Name) then 
+        if v[client.Name].team ~= nil then 
+            data = v
         end
     end
-    
-    return new_table
 end
-
-local plr_table = sort_table(holder)
 
 -- // Esp Stuff \\ --
 local function draw(obj, prop)
@@ -40,37 +30,76 @@ local function draw(obj, prop)
 end
 
 local function esp_main(obj)
-    local plr_name = plr_table[obj]
-    local text = draw('Text', {Size = 14, Color = Color3.fromRGB(255,255,255), Outline = true})
-    local tracer = draw('Line', {Thickness = 1, Color = Color3.fromRGB(255,255,255)})
+    local plr_name = holder[obj.Name]
+    local esp_cache = {}
     
-    local loop = game:GetService('RunService').RenderStepped:Connect(function()
-        if game:GetService('Players'):FindFirstChild(plr_name) then 
-            if characters:FindFirstChild(obj) then 
-                local char = characters:FindFirstChild(obj)
-                local pos, onscreen = camera:WorldToViewportPoint((char.hitbox.CFrame * CFrame.new(-1,3,0)).p)
-                local tracer_pos = camera:WorldToViewportPoint((char.hitbox.CFrame * CFrame.new(-1,-3,0)).p)
+    esp_cache.text = draw('Text', {Size = 14, Color = Color3.fromRGB(255,0,0), Outline = true})
+    esp_cache.tracer = draw('Line', {Thickness = 1, Color = Color3.fromRGB(255,0,0)})
+    
+    esp_cache.line_tl = draw('Line', {Thickness = 1, Color = Color3.fromRGB(255,0,0)})
+    esp_cache.line_tr = draw('Line', {Thickness = 1, Color = Color3.fromRGB(255,0,0)})
+    esp_cache.line_bl = draw('Line', {Thickness = 1, Color = Color3.fromRGB(255,0,0)})
+    esp_cache.line_br = draw('Line', {Thickness = 1, Color = Color3.fromRGB(255,0,0)})
+    
+    local loop
+    loop = game:GetService('RunService').RenderStepped:Connect(function()
+        if game:GetService('Players'):FindFirstChild(plr_name) ~= nil then 
+            if characters:FindFirstChild(obj.Name) and data[client.Name].team ~= data[plr_name].team then 
+                local char = characters:FindFirstChild(obj.Name)
+                local pos, onscreen = camera:WorldToViewportPoint((char.head.CFrame * CFrame.new(0,3,0)).p)
+                local tracer_pos = camera:WorldToViewportPoint((char.head.CFrame * CFrame.new(0,-3,0)).p)
                 
-                text.Text = plr_name
-                text.Position = Vector2.new(pos.x, pos.y) - Vector2.new(0, text.TextBounds.y)
-                text.Visible = onscreen
+                local tl = camera:WorldToViewportPoint((char.head.CFrame * CFrame.new(-2,3,0)).p)
+                local tr = camera:WorldToViewportPoint((char.head.CFrame * CFrame.new(2,3,0)).p)
+                local bl = camera:WorldToViewportPoint((char.head.CFrame * CFrame.new(-2,-3,0)).p)
+                local br = camera:WorldToViewportPoint((char.head.CFrame * CFrame.new(2,-3,0)).p)
                 
-                tracer.From = Vector2.new(camera.ViewportSize.x / 2, camera.ViewportSize.y / 1)
-                tracer.To = Vector2.new(tracer_pos.x, tracer_pos.y)
-                tracer.Visible = onscreen
+                esp_cache.text.Text = plr_name
+                esp_cache.text.Position = Vector2.new(pos.x, pos.y) - Vector2.new(0, esp_cache.text.TextBounds.y)
+                esp_cache.text.Visible = onscreen
+                
+                esp_cache.tracer.From = Vector2.new(camera.ViewportSize.x / 2, camera.ViewportSize.y / 1)
+                esp_cache.tracer.To = Vector2.new(tracer_pos.x, tracer_pos.y)
+                esp_cache.tracer.Visible = onscreen
+                
+                esp_cache.line_tl.From = Vector2.new(tl.x, tl.y)
+                esp_cache.line_tl.To = Vector2.new(tr.x, tr.y)
+                esp_cache.line_tl.Visible = onscreen
+                
+                esp_cache.line_tr.From = Vector2.new(tr.x, tr.y)
+                esp_cache.line_tr.To = Vector2.new(br.x, br.y)
+                esp_cache.line_tr.Visible = onscreen
+                
+                esp_cache.line_bl.From = Vector2.new(bl.x, bl.y)
+                esp_cache.line_bl.To = Vector2.new(tl.x, tl.y)
+                esp_cache.line_bl.Visible = onscreen
+                
+                esp_cache.line_br.From = Vector2.new(br.x, br.y)
+                esp_cache.line_br.To = Vector2.new(bl.x, bl.y)
+                esp_cache.line_br.Visible = onscreen
             else
-                text:Remove()
-                tracer:Remove()
+                for i,v in pairs(esp_cache) do 
+                    v:Remove()
+                end
                 if loop then loop:Disconnect() end
             end
         else
-            text:Remove()
-            tracer:Remove()
+            for i,v in pairs(esp_cache) do 
+                v:Remove()
+            end
             if loop then loop:Disconnect() end
         end
     end)
 end
 
-for i,v in pairs(plr_table) do 
-    esp_main(i)
+for i,v in pairs(game:GetService("Workspace").characters:GetChildren()) do 
+    if holder[v.Name] then 
+        esp_main(v)
+    end
 end
+
+game:GetService("Workspace").characters.ChildAdded:Connect(function(v)
+    if holder[v.Name] then 
+        esp_main(v)
+    end
+end)
